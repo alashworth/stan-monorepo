04453f7d6a74cf8059c904e2ccac8913b4257c72
Andrew Ashworth <andrew@ashworth.us>
2019-07-27 05:58:50 -0400

    Removed duplicate test.

--- tests/math_unit/math/prim/arr/fun/sum_test.cpp
+++ tests/math_unit/math/prim/arr/fun/sum_test.cpp
@@ -19,16 +19,6 @@ TEST(MathFunctions, sum) {
   EXPECT_FLOAT_EQ(6.0, stan::math::sum(x));
 }
 
-TEST(MathFunctions, sub_nan) {
-  double nan = std::numeric_limits<double>::quiet_NaN();
-  std::vector<double> x(3);
-
-  x[0] = 1.0;
-  x[1] = 2.0;
-  x[2] = nan;
-
-  EXPECT_PRED1(boost::math::isnan<double>, stan::math::sum(x));
-}
 TEST(MathMatrix, sum_vector_int) {
   std::vector<int> x(3);
   EXPECT_EQ(0, stan::math::sum(x));
