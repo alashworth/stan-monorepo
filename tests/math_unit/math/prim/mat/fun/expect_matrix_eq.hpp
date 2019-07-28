d4b6b64a1349713b2f4654a5e114185c4bfef092
Andrew Ashworth <andrew@ashworth.us>
2019-07-28 06:15:22 -0400

    Split header into header and source.

--- tests/math_unit/math/prim/mat/fun/expect_matrix_eq.hpp
+++ tests/math_unit/math/prim/mat/fun/expect_matrix_eq.hpp
@@ -2,15 +2,10 @@
 #define TEST_MATH_MATRIX_EXPECT_MATRIX_EQ_HPP
 
 #include <gtest/gtest.h>
+#include <Eigen/dense>
 
 void expect_matrix_eq(
     const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>& a,
-    const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>& b) {
-  EXPECT_EQ(a.rows(), b.rows());
-  EXPECT_EQ(a.cols(), b.cols());
-  for (int i = 0; i < a.rows(); ++i)
-    for (int j = 0; j < a.cols(); ++j)
-      EXPECT_FLOAT_EQ(a(i, j), b(i, j));
-}
+    const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>& b);
 
 #endif
